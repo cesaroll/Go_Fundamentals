ea488d8da3fc26520dff480310a44f893a69c919
cesaroll <cesarlerdeut@gmail.com>
2025-01-19 21:23:02 +0100

    Loop tiol condition

--- Loops/main.go
+++ Loops/main.go
@@ -13,18 +13,37 @@ func main() {
 	}
 
 	scores := []Score{}
+	shouldContinue := true
 
-	for {
-		fmt.Print("Enter student name and score: ")
-		var name, rawScore string
-		fmt.Scanln(&name, &rawScore)
-		score, err := strconv.Atoi(rawScore)
-		if err != nil {
-			fmt.Println("Invalid score")
-			continue
+	for shouldContinue {
+
+		fmt.Println("(e) Enter a student\n(p) Print all students\n(q) Quit")
+		var option string
+		fmt.Scanln(&option)
+
+		switch option {
+			case "e":
+				fmt.Print("\nEnter student name and score: ")
+				var name, rawScore string
+				fmt.Scanln(&name, &rawScore)
+				score, err := strconv.Atoi(rawScore)
+				if err != nil {
+					fmt.Println("Invalid score")
+					continue
+				}
+				scores = append(scores, Score{Name: name, Score: score})
+
+			case "p":
+				fmt.Println()
+				for _, score := range scores {
+					fmt.Println(score.Name, " - ", score.Score)
+				}
+
+			case "q":
+				shouldContinue = false
+			default:
+				shouldContinue = false
 		}
-		fmt.Println(name, score)
-		scores = append(scores, Score{Name: name, Score: score})
+		fmt.Println()
 	}
-
 }
