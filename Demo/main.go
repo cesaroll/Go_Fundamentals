d5b0de93ec2d7f7e54d97de0c2b442ea5bd65330
cesaroll <cesarlerdeut@gmail.com>
2025-01-19 16:01:41 +0100

    Using Slices

--- Demo/main.go
+++ Demo/main.go
@@ -12,9 +12,19 @@ func main() {
 	// var score = 99;
 	// score := 99;
 
-	name, score := "Cesar", 99
+	// name, score := "Cesar", 99
+
+	students := []string{
+		"Cesar",
+		"Denise",
+		"Jocelyn",
+	}
+
+	scores := []int{99,93,96}
 
 	fmt.Println("Student scores")
 	fmt.Println(strings.Repeat("-", 16))
-	fmt.Println(name, score);
+	fmt.Println(students[0], scores[0]);
+	fmt.Println(students[1], scores[1]);
+	fmt.Println(students[2], scores[2]);
 }
