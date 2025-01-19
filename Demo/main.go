ee41ba75c264a4dba7da63493bb8654dc1b8469a
cesaroll <cesarlerdeut@gmail.com>
2025-01-19 16:24:55 +0100

    Using structs

--- Demo/main.go
+++ Demo/main.go
@@ -5,40 +5,22 @@ import (
 	"strings"
 )
 
+type Score struct {
+	Name	string
+	Score	int
+}
+
 func main() {
 	fmt.Println("Student scores")
 	fmt.Println(strings.Repeat("-", 16))
 
-	// var name string = "Cesar";
-	// name := "Cesar";
-
-	// var score = 99;
-	// score := 99;
-
-	// name, score := "Cesar", 99
-
-	// Slices
-	// students := []string{
-	// 	"Cesar",
-	// 	"Denise",
-	// 	"Jocelyn",
-	// }
-
-	// scores := []int{99,93,96}
-
-	// fmt.Println(students[0], scores[0]);
-	// fmt.Println(students[1], scores[1]);
-	// fmt.Println(students[2], scores[2]);
-
-	// Maps
-	scores := map[string]int{
-		"Cesar": 99,
-		"Denise": 93,
-		"Jocelyn": 96,
+	scores := []Score{
+		{Name: "Cesar", Score: 99},
+		{"Denise", 98},
+		{"Jocelyn", 97},
 	}
 
-	fmt.Println(scores)
-
-	scores["Denise"] = 96
-	fmt.Println("Denise: ", scores["Denise"])
+	for _, score := range scores{
+		fmt.Printf("%s %d\n", score.Name, score.Score)
+	}
 }
